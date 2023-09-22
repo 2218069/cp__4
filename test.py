182dc82f4949a19630fc41a1cf3621ed6bc8749d
2218069 <amno0334a@naver.com>
2023-09-22 16:11:22 +0900

    컴퓨터프로그래밍 4주차
    score이라는 변수에 학생들의 점수를 저리스트로 저장하고 for문을 사용하여 학생들의 점수 환산을 나타낸다

---
+++ test.py
@@ -0,0 +1,12 @@
+score = [90, 45, 64, 9, 17, 29]
+result=[]
+for i in score:
+    if i >= 71:
+        result.append ('A')
+    elif i >= 41:
+        result.append ('B')
+    elif i >= 11:
+        result.append ('C')
+    else:
+        result.append ('D')
+print(result)
